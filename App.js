ac0abf5567b7ba9a6f1a10d62f70c006468344dc
Luckner CYNÉ <luckner@cyne.fr>
2023-07-10 12:14:20 +0200

    feat: set up bitmovin for a dev client build

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useCallback } from "react";
+import { View, Platform, StyleSheet } from "react-native";
+import {
+  usePlayer,
+  SourceType,
+  PlayerView,
+} from "bitmovin-player-react-native";
 
-export default function App() {
+export default function PlayerSample() {
+  const player = usePlayer({
+    // The only required parameter is the license key but it can be omitted from code upon correct
+    // Info.plist/AndroidManifest.xml configuration.
+    //
+    // Head to `Configuring your License` for more information.
+    licenseKey: "<ENTER-YOUR-LICENSE-KEY>",
+  });
+
+  useEffect(() => {
+    player.load({
+      url: "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+      type: Platform.OS === "ios" ? SourceType.HLS : SourceType.DASH,
+      title: "Art of Motion",
+    });
+  }, [player]);
+
+  // onReady is called when the player has downloaded initial
+  // video and audio and is ready to start playback.
+  const onReady = useCallback(
+    (event) => {
+      // Start playback
+      player.play();
+      console.log(event.timestamp);
+    },
+    [player]
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.flex1}>
+      <PlayerView style={styles.flex1} player={player} onReady={onReady} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  flex1: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
